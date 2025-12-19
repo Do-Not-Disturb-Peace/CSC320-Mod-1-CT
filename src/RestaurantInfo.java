c41192449c82f713ab9ecc06f9b958d47d02704e
Do-Not-Disturb-Peace <t_elli07@outlook.com>
2025-12-18 22:20:04 -0600

    Add Module 1 Java source code

---
+++ src/RestaurantInfo.java
@@ -0,0 +1,13 @@
+public class RestaurantInfo {
+    public static void main(String[] args) {
+        String restaurantName = "Curry 3";
+        String businessAddress = "SanFran St";
+        String city = "DUB City";
+        String state = "CA";
+        String zipCode = "0000";
+
+        System.out.println(restaurantName);
+        System.out.println(businessAddress);
+        System.out.println(city + ", " + state + " " + zipCode);
+    }
+}
